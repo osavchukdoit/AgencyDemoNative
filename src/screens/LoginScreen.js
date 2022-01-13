95356064a7552b1bcec26ca913e2d3cf8532b474
Oleh-Li <Oleh.7ee@gmail.com>
2022-01-13 21:16:27 +0200

    add react-native-linear-gradient library

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import { Button, Text, View, StyleSheet, TextInput } from "react-native";
+import {TouchableOpacity, Button, Text, View, StyleSheet, TextInput, linearGradient } from "react-native";
 import { THEME } from "../styles/theme";
-import {login} from "../api/login";
+import { login } from "../api/login";
+import { borderBottomColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
 
 export const LoginScreen = ({ navigation }) => {
   const [userDetails, setUserDetails] = useState({ login: "", password: "" });
@@ -12,45 +13,79 @@ export const LoginScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.switchOnline}>
-        <Text>online/offline</Text>
-      </View>
-
-      <View>
-        <Text>Please log in to your account:</Text>
+      <View style={styles.loginWindow}>
+        <Text style={styles.loginWimdow_title}>
+          Please log in to your account:
+        </Text>
         <TextInput
+          style={styles.textInput}
           onChangeText={(text) => handleInputValue("login", text)}
-          placeholder={"Login"}
+          placeholder={"Username"}
           autoCapitalize={"none"}
         />
         <TextInput
+          style={styles.textInput}
           onChangeText={(text) => handleInputValue("password", text)}
           placeholder={"Password"}
           autoCapitalize={"none"}
         />
-        <Button
-          title={"Login"}
-          onPress={() =>
-            // navigation.navigate("Employers", { name: "Local User" })
-            // login(userDetails)
-            console.log(userDetails)
-          }
-        />
+        <View style={styles.wraper_login_button}>
+          <Button
+            title={"Sign in to agencyEZ"}
+            onPress={() =>
+              // navigation.navigate("Employers", { name: "Local User" })
+              // login(userDetails)
+              console.log(userDetails)
+            }
+          />
+        </View>
       </View>
     </View>
   );
 };
+
+
+/*
+-вынести цвета в переменые
+-втавить иконки в поля юзернаме и пасворд
+-fontweight in title
+- no capitalized in title button
+*/
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: THEME.BACKGROUND.MAIN,
     flex: 1,
     justifyContent: "center",
+    alignItems: "center",
     position: "relative",
   },
-  switchOnline: {
-    position: "absolute",
-    top: 0,
-    right: 0,
+  loginWindow: {
+    backgroundColor: "#fff",
+    width: "80%",
+    height: "50%",
+    paddingTop: "8%",
+    alignItems: "center",
+  },
+  loginWimdow_title: {
+    fontSize: 32,
+    color: THEME.COLOR.LOGIN_TITLE,
+    marginBottom: 20,
+    fontWeight: "bold"
+  },
+
+  textInput: {
+    borderWidth: 1,
+    borderColor: THEME.COLOR.INPUT_BORDER,
+    borderRadius: 4,
+    marginBottom: 15,
+    height: 30,
+    paddingLeft: 15,
+    width: "78%",
+    
+  },
+
+  wraper_login_button: {
+    width: "78%",
   },
 });
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,5 +1,10 @@
 export const THEME = {
-  COLOR: {},
+  COLOR: {
+    LOGIN_TITLE: "#212529",
+    INPUT_BORDER: "#429008",
+    INPUT_BORDER_HOVER: "#53B50A",
+    INPUT_TEXT_COLOR: "#2E2E2E",
+  },
   BACKGROUND: {
     MAIN: "#f4efef",
   },
