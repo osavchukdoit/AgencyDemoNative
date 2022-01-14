371ef67d03081022c3b9d4623ef1ceb20c93733c
Oleh-Li <Oleh.7ee@gmail.com>
2022-01-14 13:51:53 +0200

    uninstall linear-gradient and try fix it again

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import {TouchableOpacity, Button, Text, View, StyleSheet, TextInput, linearGradient } from "react-native";
-import { THEME } from "../styles/theme";
+import { Button, Text, View, TextInput } from "react-native";
 import { login } from "../api/login";
-import { borderBottomColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
-import styles from "../styles/loginScreenStyles"
+import styles from "../styles/loginScreenStyles";
+import { LinearGradient } from "react-native-linear-gradient"; 
+
 
 export const LoginScreen = ({ navigation }) => {
   const [userDetails, setUserDetails] = useState({ login: "", password: "" });
@@ -45,48 +45,9 @@ export const LoginScreen = ({ navigation }) => {
   );
 };
 
-
 /*
 -вынести цвета в переменые
 -втавить иконки в поля юзернаме и пасворд
 -fontweight in title
 - no capitalized in title button
 */
-
-// const styles = StyleSheet.create({
-//   container: {
-//     backgroundColor: THEME.BACKGROUND.MAIN,
-//     flex: 1,
-//     justifyContent: "center",
-//     alignItems: "center",
-//     position: "relative",
-//   },
-//   loginWindow: {
-//     backgroundColor: "#fff",
-//     width: "80%",
-//     height: "50%",
-//     paddingTop: "8%",
-//     alignItems: "center",
-//   },
-//   loginWimdow_title: {
-//     fontSize: 32,
-//     color: THEME.COLOR.LOGIN_TITLE,
-//     marginBottom: 20,
-//     fontWeight: "bold"
-//   },
-
-//   textInput: {
-//     borderWidth: 1,
-//     borderColor: THEME.COLOR.INPUT_BORDER,
-//     borderRadius: 4,
-//     marginBottom: 15,
-//     height: 30,
-//     paddingLeft: 15,
-//     width: "78%",
-    
-//   },
-
-//   wraper_login_button: {
-//     width: "78%",
-//   },
-// });
