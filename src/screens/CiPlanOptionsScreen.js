871de4d2478357b3d811e926d623d7bfbc52a1ad
Oleh-Li <Oleh.7ee@gmail.com>
2022-03-11 12:22:43 +0200

    added CiInfoProductDescription component markup

--- src/screens/CiPlanOptionsScreen.js
+++ src/screens/CiPlanOptionsScreen.js
@@ -2,16 +2,21 @@ import { View, ScrollView } from "react-native";
 import { CiPlanOptionsTop } from "../components/ciPlanOptionsPage/CiPlanOptionsTop";
 import { ButtonsPlanAndMoreInfo } from "../components/stdPlanOptions/ButtonsPlanAndMoreInfo";
 import { CiPlanSector } from "../components/ciPlanOptionsPage/CiPlanSector";
-
+import { ButtonsMoreInfo } from "../components/stdPlanOptions/ButtonsMoreInfo";
+import { CiInfoProductDescription } from "../components/ciPlanOptionsPage/CiInfoProductDescription";
+import { ButtonBenefitsCart } from "../components/familyInfoPage/ButtonBenefitsCart";
 export const CiPlanOptionsScreen = () => {
   return (
     <View>
       <ScrollView>
         <CiPlanOptionsTop />
         <ButtonsPlanAndMoreInfo />
+        <ButtonsMoreInfo />
+        <CiInfoProductDescription />
         <CiPlanSector />
         <CiPlanSector />
       </ScrollView>
+      <ButtonBenefitsCart />
     </View>
   );
 };
