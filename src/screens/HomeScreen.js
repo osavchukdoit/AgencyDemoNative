2885bd0d2fcd79df50a1784703c7d380334fd661
osavchukdoit <o.savchuk@do-it.co>
2022-03-16 14:40:44 +0200

    fixed home screen markup

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { ScrollView, View } from "react-native";
+import { ScrollView, View, StyleSheet } from "react-native";
 import { BenefitGroups } from "../components/homePage/BenefitGroups";
 import { HomePageTop } from "../components/homePage/HomePageTop";
 import { ButtonEnrollment } from "../components/homePage/ButtonEnrollment";
 
 export const HomeScreen = ({ navigation }) => {
   return (
-    <View>
-      <ScrollView stickyHeaderIndices={[0]}>
+    <View style={styles.wrapper}>
+      <ScrollView stickyHeaderIndices={[0]} style={styles.scrollContainer}>
         <HomePageTop />
         <View>
           <BenefitGroups />
@@ -24,3 +24,12 @@ export const HomeScreen = ({ navigation }) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  wrapper: {
+    height: "100%",
+  },
+  scrollContainer: {
+    marginBottom: 70,
+  },
+});
